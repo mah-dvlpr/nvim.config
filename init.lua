39e5581d83c5c11efc29538910ee2c38bd1150fc
mah-dvlpr <mah.dvlpr@gmail.com>
2021-09-23 21:13:36 +0200

    Remove plugin i never really used.

--- init.lua
+++ init.lua
@@ -84,7 +84,6 @@ require('packer').startup({
         use { 'morhetz/gruvbox' }
         use { 'hoob3rt/lualine.nvim' }
         use { 'dstein64/nvim-scrollview' }
-        use { 'karb94/neoscroll.nvim' }
     end
     ,
     config = {
@@ -318,21 +317,4 @@ require('lualine').setup({
     lualine_c = {'filename'},
     lualine_x = {'location'},
   },
-})
-
-
-----------------------------------------------------------------------------------------------------
--- karb94/neoscroll.nvim
-require('neoscroll').setup({
-    -- All these keys will be mapped to their corresponding default scrolling animation
-    mappings = {'<C-u>', '<C-d>', '<C-b>', '<C-f>',
-                '<C-y>', '<C-e>', 'zt', 'zz', 'zb'},
-    hide_cursor = true,          -- Hide cursor while scrolling
-    stop_eof = true,             -- Stop at <EOF> when scrolling downwards
-    use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
-    respect_scrolloff = true,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
-    cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-    easing_function = 'circular',        -- Default easing function
-    pre_hook = nil,              -- Function to run before the scrolling animation starts
-    post_hook = nil,              -- Function to run after the scrolling animation ends
 })
