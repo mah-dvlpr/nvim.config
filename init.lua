5ef3882e8b568780f45b910dd0d828ba2594d4de
mah-dvlpr <mah.dvlpr@gmail.com>
2022-05-30 01:51:04 +0200

    Minor fixes.

--- init.lua
+++ init.lua
@@ -1,7 +1,25 @@
 -- ================================================================
+-- Custom (Neo)Vim definitions
+function map(mode, lhs, rhs, opts)
+    local options = { noremap = true }
+    if opts then
+        options = vim.tbl_extend("force", options, opts)
+    end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+function map_buf(bufnr, mode, lhs, rhs, opts)
+    local options = { noremap = true }
+    if opts then
+        options = vim.tbl_extend("force", options, opts)
+    end
+    vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, options)
+end
+
+-- ================================================================
 -- (Neo)Vim options
 vim.opt.clipboard = 'unnamedplus'         -- Enable system clipboard
-vim.opt.syntax = 'on'                     -- Enable syntax linting (not needed with an LSP?)
+--vim.opt.syntax = 'on'                     -- Enable syntax linting (not needed with an LSP?)
 vim.opt.termguicolors = true              -- Use true colors, instead oluf just usual 256-bit colors.
 vim.opt.wrap = false                      -- Do not wrap lines, plain and simple.
 vim.opt.number = true                     -- Absolute numbering.
@@ -21,13 +39,19 @@ vim.opt.hidden = true                     -- Keep buffers open when switching be
 -- Plugins
 local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  packer_bootstrap = vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    packer_bootstrap = vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 end
 
-vim.cmd [[packadd packer.nvim]]
-return require('packer').startup(function()
-    use 'wbthomason/packer.nvim'
+vim.cmd[[packadd packer.nvim]]
+require('packer').startup(function(use)
+    use {
+        'wbthomason/packer.nvim'
+    }
+end)
 
+-- ================================================================
+-- nvim-lsp-installer (HAS TO BE RUN BEFORE lspconfig!)
+require('packer').startup(function(use)
     use {
         "williamboman/nvim-lsp-installer",
         {
@@ -41,18 +65,61 @@ return require('packer').startup(function()
     }
 end)
 
--- ================================================================
--- nvim-lsp-installer (HAS TO BE RUN BEFORE lspconfig!)
-require("nvim-lsp-installer").setup({
-    automatic_installation = true, -- automatically detect which servers to install (based on which servers are set up via lspconfig)
-    ui = {
-        icons = {
-            server_installed = "✓",
-            server_pending = "➜",
-            server_uninstalled = "✗"
-        }
-    }
-})
+--require("nvim-lsp-installer").setup({
+--    automatic_installation = true, -- automatically detect which servers to install (based on which servers are set up via lspconfig)
+--    ui = {
+--        icons = {
+--            server_installed = "✓",
+--            server_pending = "➜",
+--            server_uninstalled = "✗"
+--        }
+--    }
+--})
 
 -- ================================================================
 -- lspconfig
+require('packer').startup(function(use)
+    use 'wbthomason/packer.nvim' -- Package manager
+    use 'neovim/nvim-lspconfig' -- Collection of configurations for the built-in LSP client
+end)
+
+---- Mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+local opts = { noremap=true, silent=true }
+map('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
+map('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>', opts)
+map('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>', opts)
+map('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<cr>', opts)
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+    -- Enable completion triggered by <c-x><c-o>
+    --
+    vim.bo(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+    -- Mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    map_buf(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+    map_buf(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+    map_buf(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+    map_buf(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+    map_buf(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+    map_buf(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<cr>', opts)
+    map_buf(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<cr>', opts)
+    map_buf(bufnr, 'n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<cr>', opts)
+    map_buf(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+    map_buf(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+    map_buf(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+    map_buf(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+    map_buf(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<cr>', opts)
+end
+
+-- Use a loop to conveniently call 'setup' on multiple servers and
+-- map buffer local keybindings when the language server attaches
+local servers = { 'rust_analyzer', 'tsserver' }
+for _, lsp in pairs(servers) do
+    require('lspconfig')[lsp].setup {
+      on_attach = on_attach,
+    }
+end
