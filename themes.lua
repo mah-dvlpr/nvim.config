96e3b273b3c304bcdc853ca3c561da4b80bfa44d
mah-dvlpr <mah.dvlpr@gmail.com>
2022-05-30 12:04:58 +0200

    ...

--- themes.lua
+++ themes.lua
@@ -3,9 +3,9 @@ require('global')
 local vscode = {
   config = function()
     vim.o.background = 'dark'
-    vim.g.vscode_transparent = 1
+    --vim.g.vscode_transparent = 1
     vim.g.vscode_italic_comment = 1
-    vim.g.vscode_disable_nvimtree_bg = true
+    --vim.g.vscode_disable_nvimtree_bg = true
     vim.cmd([[colorscheme vscode]])
   end,
 }
